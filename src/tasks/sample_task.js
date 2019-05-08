88902e21458318e3d343037492541fd4c2b326d9
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2019-05-09 00:53:43 +0100

    get remote

--- src/tasks/sample_task.js
+++ src/tasks/sample_task.js
@@ -1,20 +1,53 @@
 #!/usr/bin/env node
+
 'use strict';
 
 const Chalk = require('chalk');
 const log = console.log;
 const fs = require('fs');
 const Utils = require('../utils/utils');
+const remoteGitTags = require('remote-git-tags');
+const semverSort = require('semver-sort');
+const origin = require('remote-origin-url');
+
+
+function sanitizeRemote(remote) {
+    // "git@github.com:cesarferreira/git-tag-cli.git"
+
+    if (!remote.includes('git@')) return remote
+
+    return 'github.com/' + remote.split(':')[1].replace('.git', '')
+}
 
 // Main code //
 const self = module.exports = {
-	init: input => {
+    init: input => {
 
-		if (input.length == 0) {
-			log(Chalk.red(`You need to specify a params`));
-			return;
-		}
-	
-		log(`sample task with: ${input[0]}`);
-	}
+        if (input.length == 0) {
+            log(Chalk.red(`You need to specify a params`));
+            return;
+        }
+
+        var remote = "";
+
+        (async() => {
+            let url = await origin();
+            remote = sanitizeRemote(url)
+            log(remote);
+            // url => "https://github.com/jonschlinkert/remote-origin-url.git"
+        })();
+
+        remoteGitTags('github.com/GlueHome/common-android')
+            .then(tags => {
+                let versions = [...tags.keys()];
+                // log(versions)
+                semverSort.desc(versions);
+                // log(versions)
+
+                log(`Latest is: ${versions[0]}`)
+
+            });
+
+        // log(`sample task with: ${input[0]}`);
+    }
 };
