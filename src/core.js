cf771c045734d3e1aa891f394b55377de3e44d15
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2019-12-20 02:05:15 +0000

    added release notes capability

--- src/core.js
+++ src/core.js
@@ -1,39 +1,42 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk');
-const Utils = require('./utils');
+const chalk = require("chalk");
+const Utils = require("./utils");
 const log = console.log;
-const ui = require('./ui');
-const ora = require('ora');
-const pkg = require('../package.json');
-const githubUrlFromGit = require('github-url-from-git');
-const gitRemoteOriginUrl = require('git-remote-origin-url');
+const ui = require("./ui");
+const ora = require("ora");
+const pkg = require("../package.json");
+const githubUrlFromGit = require("github-url-from-git");
+const gitRemoteOriginUrl = require("git-remote-origin-url");
 
 async function areYouSureYouWantToPush(oldVersion, newTag, message) {
-    const answersConfirmation = await ui.askForConfirmation(oldVersion, newTag)
+    const answersConfirmation = await ui.askForConfirmation(oldVersion, newTag);
 
-    log()
-    if (answersConfirmation['confirm']) {
+    log();
+    if (answersConfirmation["confirm"]) {
         const spinner = ora(`Pushing ${chalk.bold.green(newTag)}`).start();
         try {
-            await Utils.pushNewTag(newTag, message)
-            spinner.succeed(ui.tagPushSuccessMessage(newTag))
+            await Utils.pushNewTag(newTag, message);
+            spinner.succeed(ui.tagPushSuccessMessage(newTag));
             return true;
         } catch (error) {
-            spinner.fail(error)
+            spinner.fail(error);
             return false;
         }
     } else {
-        ui.failsToConfirm()
+        ui.failsToConfirm();
         return false;
     }
 }
 
 async function createRelease(oldTag, newTag) {
-
-    const remote = await gitRemoteOriginUrl()
-    const repoUrl = githubUrlFromGit(remote)
-    const { hasCommits, releaseNotes } = await Utils.printCommitLog(repoUrl, oldTag, newTag);
+    const remote = await gitRemoteOriginUrl();
+    const repoUrl = githubUrlFromGit(remote);
+    const { hasCommits, releaseNotes } = await Utils.printCommitLog(
+        repoUrl,
+        oldTag,
+        newTag
+    );
 
     const options = {
         oldTag,
@@ -41,47 +44,85 @@ async function createRelease(oldTag, newTag) {
         repoUrl,
         hasCommits,
         releaseNotes
-    }
+    };
 
-    await Utils.releaseTaskHelper(options)
+    await Utils.releaseTaskHelper(options);
+}
+
+async function getReleaseNotes(oldTag, newTag) {
+    const remote = await gitRemoteOriginUrl();
+    const repoUrl = githubUrlFromGit(remote);
+    const { hasCommits, releaseNotes } = await Utils.printCommitLog(
+        repoUrl,
+        oldTag,
+        newTag
+    );
+
+    return releaseNotes;
 }
 
 // Main code //
 module.exports = {
     init: (input, flags) => {
-
         const command = input[0] || "";
         const params = input.subarray(1, input.length);
 
         switch (command.toLowerCase()) {
-            case 'major':
-            case 'minor':
-            case 'patch':
-            case 'prepatch':
-            case 'preminor':
-            case 'premajor':
-            case 'prerelease':
+            case "major":
+            case "minor":
+            case "patch":
+            case "prepatch":
+            case "preminor":
+            case "premajor":
+            case "prerelease":
                 (async() => {
-                    const oldTag = await Utils.getLatestTag()
-                    const newTag = await Utils.getNextVersionFor(oldTag, command.toLowerCase())
-                    const confirmed = await areYouSureYouWantToPush(oldTag, newTag, newTag)
-                    if (confirmed) await createRelease(oldTag, newTag)
+                    const oldTag = await Utils.getLatestTag();
+                    const newTag = await Utils.getNextVersionFor(
+                        oldTag,
+                        command.toLowerCase()
+                    );
+                    const confirmed = await areYouSureYouWantToPush(
+                        oldTag,
+                        newTag,
+                        newTag
+                    );
+                    if (confirmed) await createRelease(oldTag, newTag);
                 })();
                 break;
-            case 'about':
-                ui.printAbout()
+            case "about":
+                ui.printAbout();
                 break;
-            case 'version':
-                log(`Current version is ${Chalk.green(pkg.version)}`)
+            case "version":
+                log(`Current version is ${chalk.green(pkg.version)}`);
+                break;
+
+            case "commits":
+            case "releasenotes":
+            case "notes":
+                (async() => {
+                    const releaseNotes = await getReleaseNotes(await Utils.getLatestTag(), "HEAD")
+
+                    log(chalk.white.bold("Commits:"))
+                    log(releaseNotes)
+                })();
                 break;
             default:
                 (async() => {
-                    var oldTag = await Utils.getLatestTag()
-                    ui.initialPrompt(oldTag)
+                    var oldTag = await Utils.getLatestTag();
+                    ui.initialPrompt(oldTag);
+                    // x.substring(0, x.lastIndexOf("\n"));
+                    log(chalk.white.bold("Commits:"))
+                    const releaseNotes = await getReleaseNotes(await Utils.getLatestTag(), "HEAD")
+                    log(releaseNotes.substring(0, releaseNotes.lastIndexOf("\n")))
+
                     const { newTag, message } = await ui.askForValidNewTag(oldTag);
-                    const confirmed = await areYouSureYouWantToPush(oldTag, newTag, message)
-                    if (confirmed) await createRelease(oldTag, newTag)
-                })()
+                    const confirmed = await areYouSureYouWantToPush(
+                        oldTag,
+                        newTag,
+                        message
+                    );
+                    if (confirmed) await createRelease(oldTag, newTag);
+                })();
         }
     }
 };
